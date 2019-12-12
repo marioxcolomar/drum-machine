2acd1d902f40d1699430db22198213626aa75d36
marioxcolomar <marioxcolomar@gmail.com>
2019-12-12 23:43:17 +0100

    WIP: access audio tag and play the source file.

--- src/components/DrumPad.tsx
+++ src/components/DrumPad.tsx
@@ -1,30 +1,32 @@
-import * as React from 'react'
+import * as React from 'react';
 
 interface DrumPadProps {
-	SoundId: string,
-	KeyString: string,
+	SoundId: string;
+	KeyString: string;
+	KeyCode: number;
 }
 
-const DrumPad: React.FC<DrumPadProps> =
-	({ SoundId, KeyString }) => {
+const DrumPad: React.FC<DrumPadProps> = ({ SoundId, KeyString, KeyCode }) => {
+	const audioRef = React.createRef<HTMLAudioElement>()
 
 	const drumPadStyles: React.CSSProperties = {
 		padding: '10px',
 		border: '1px solid lightgrey',
 		borderRadius: '5px',
-		width: '40px',
-		height: '40px',
+		width: '50px',
+		height: '50px',
 		cursor: 'pointer',
 		margin: 'auto',
-		fontWeight: 500,
-	}
+		fontWeight: 500
+	};
 
 	return (
-	<div id={SoundId} className='drum-pad' style={drumPadStyles}>
-		<kbd>{KeyString.toUpperCase()}</kbd>
-		<audio src={`../assets/${SoundId}.wav`} />
-	</div>
-	)
-}
+		<div id={SoundId} className='drum-pad' style={drumPadStyles} data-key={KeyCode} onClick={() => audioRef}>
+			<kbd>{KeyString.toUpperCase()}</kbd>
+			<p style={{ fontSize: '12px' }}>{SoundId}</p>
+			<audio className='clip' id={KeyString.toUpperCase()} src={`../assets/${SoundId}.wav`} ref={audioRef} autoPlay />
+		</div>
+	);
+};
 
-export default DrumPad
+export default DrumPad;
