a5338c634c30c2c134d32004f0553fc57b78988c
marioxcolomar <marioxcolomar@gmail.com>
2019-12-14 21:51:00 +0100

    Add array of drum pads.

---
+++ src/assets/drumPads.tsx
@@ -0,0 +1,11 @@
+export default [
+  {letter: 'Q', name: 'boom', key: 81},
+  {letter: 'W', name: 'clap', key: 87},
+  {letter: 'E', name: 'hihat', key: 69},
+  {letter: 'A', name: 'kick', key: 65},
+  {letter: 'S', name: 'openhat', key: 83},
+  {letter: 'D', name: 'ride', key: 68},
+  {letter: 'Z', name: 'snare', key: 90},
+  {letter: 'X', name: 'tink', key: 88},
+  {letter: 'C', name: 'tom', key: 67},
+]
